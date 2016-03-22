e09b9c9e084c09bccd155ea1f4f48401ac747068
Jake Streb <jake@getgrist.com>
2016-03-22 01:13:15 -0400

    Added question voting and timer

--- index.js
+++ index.js
@@ -1,102 +1,200 @@
-var dbAddr = 'https://thingswithbeth.firebaseio.com/';
-var myRef = new Firebase(dbAddr);
-var players = myRef.child('players');
+var db = new Firebase('https://thingswithbeth.firebaseio.com/');
+var data = null;
 
-var playerKey = window.location.href.split("/").pop();
+var nameInput = null;
+var interval = null;
+
+var gameKey = null;
+var gameObj = null;
+var playerKey = null;
 var playerObj = null;
 
 var responseOrder = [];
 
-var state = "respond"; // Monitors db value
-myRef.child("state").on("value", snapshot => state = snapshot.val());
-
-var round = 1; // Monitors db value
-myRef.child("round").on("value", snapshot => round = snapshot.val());
+var state = "pick"; // Monitors db value
+var round = 0; // Monitors db value
 
 var clientPlayers = {}; // The players content the client is aware of
 
-// Handles all changes to the database (expect player removals)
-myRef.on("value", snapshot => handleUpdate(snapshot));
-
-// Check if a player has been removed
-players.on("child_removed", snapshot => removePlayer(snapshot));
-
 window.onload = init;
+
+// TODO Features:
+// - Leave game
+// - End game (host)
+// - Report guessed for any response (host)
+// - List of active games to join (instead of type in)
+// - Games more than 24hr old are removed when looked up (add timestamp on new game)
+// - Scoring
 
 // Init
 function init() {
-  $('input#name').keyup(e => {
-    // Enter key
-    if (e.keyCode === 13) addPlayer();
+  // $('input#name').keyup(e => {
+  //   // Enter key
+  //   if (e.keyCode === 13) addPlayer();
+  // });
+  _loadJSON(function(response) {
+  // Parse JSON string into object
+    data = JSON.parse(response);
+  });
+
+  // Get keys from URL
+  var urlItems = window.location.hash.split("/");
+  urlItems.forEach(item => {
+    switch (item.slice(0, 2)) {
+      case "%g":
+        gameKey = item.slice(2);
+        break;
+      case "%u":
+        playerKey = item.slice(2);
+        break;
+    }
+  });
+  console.warn('keys', gameKey, playerKey);
+
+  // Initialize page based on URL
+  var games = [];
+  db.once('value').then(snapshot => {
+    games = snapshot.val();
+
+    // Retrieve game if in database, break if not
+    if (!gameKey || !games || !(gameKey in games)) {
+      window.location.hash = ""; // Clears URL suffix
+      throw "cannot retrieve game";
+    }
+    // Initialize game
+    gameObj = snapshot.child(gameKey).ref();
+    return prepareGame();
+  }).then(() => {
+    // Retrieve player if in database, break if not
+    var players = games[gameKey].players;
+    if (!playerKey || !players || !(playerKey in players)) {
+      window.location.hash = "/%g" + gameKey; // Clears player suffix
+      throw "cannot retrieve player";
+    }
+    // Initialize player prompt
+    playerObj = gameObj.child("players").child(playerKey);
+    preparePlayer();
+  }, error => {
+    console.warn(error);
   });
 }
 
 // Handles the following tasks whenever a value in the database changes:
-// - Shows join prompt if no players exist
-// - "Logs in" current user if in the database
 // - Builds/updates player list dom
 // - Builds response list dom
 // - Determines which prompt to show at the top
 function handleUpdate(snapshot) {
   // Get object containing players in the database
+  var game = snapshot.val();
   var dbPlayers = snapshot.child("players").val();
-
-  // If there are no players yet, show the "join" prompt and stop updating
-  if (!dbPlayers) {
-    _showNameContainer();
-    return;
-  }
-
-  // Retrieve player if in database
-  if (playerObj === null) {
-    if (playerKey in dbPlayers) {
-      playerObj = players.child(playerKey).ref();
-    } else {
-      window.location.hash = ""; // Clears URL suffix
-      _showNameContainer();
-    }
-  }
+  if (dbPlayers === null) return;
 
   var playersReady = true;
+  var dbPlayersKeys = Object.keys(dbPlayers);
+  var numPlayers = dbPlayersKeys.length;
 
-  Object.keys(dbPlayers).forEach(key => {
+  dbPlayersKeys.forEach(key => {
     var dbPlayer = dbPlayers[key];
     _updatePlayerDOM(key, dbPlayer);
 
     // If key isn't in responseOrder, and it`s ready, add it
-    if (responseOrder.indexOf(key) === -1 && dbPlayer.ready >= round) {
+    if (responseOrder.indexOf(key) === -1 && dbPlayer.responseReady >= round) {
       var spliceIndex = Math.floor((responseOrder.length+1)*Math.random());
       responseOrder.splice(spliceIndex, 0, key);
     }
     // If player isn't ready, not ready to display answers
-    if (dbPlayer.ready < round) playersReady = false;
+    if (dbPlayer.responseReady < round) playersReady = false;
   });
 
-  // Set to guess state if all players are ready
-  if (playersReady && state === "respond") myRef.child("state").set("guess");
+  // -----
 
-  // Build all responses from responseOrder array (always from scratch,
-  // inefficient but probably doesn`t matter)
+  // Set to guess state if all players are ready
+  if (playersReady && state === "respond")  db.child(gameKey).update({state: "guess"});
+
   if (state === "guess") {
+    // Build all responses from responseOrder array (always from scratch,
+    // inefficient but probably doesn`t matter)
+    _showResponses();
     var responses = responseOrder.map(key => {
       var player = dbPlayers[key];
       if (!player) return null; // Occurs if player is removed during guess state
       return _responseDOM(player.response, player.guessed);
     });
     $("#responses").html(responses);
-  } else {
+  }
+  else if (state === "pick") {
+    // Build all questions and markers to indicate voters (always from scratch)
+    _showQuestions();
+    $('#question').html("");
+    $('#question_a').html(game.questionA);
+    $('#question_b').html(game.questionB);
+    $('#question_c').html(game.questionC);
+
+    var a = {question: game.questionA, voters: []};
+    var b = {question: game.questionB, voters: []};
+    var c = {question: game.questionC, voters: []};
+    var numVoters = 0;
+    Object.keys(dbPlayers).forEach(key => {
+      var dbPlayer = dbPlayers[key];
+      if (dbPlayer.voteReady === round) {
+        switch (dbPlayer.vote) {
+          case "a":
+            a.voters.push(dbPlayer.name);
+            numVoters++;
+            break;
+          case "b":
+            b.voters.push(dbPlayer.name);
+            numVoters++;
+            break;
+          case "c":
+            c.voters.push(dbPlayer.name);
+            numVoters++;
+            break;
+        }
+      }
+    });
+    $('#question_a_container .voters').html(a.voters);
+    $('#question_b_container .voters').html(b.voters);
+    $('#question_c_container .voters').html(c.voters);
+
+    // If someone voted, start the countdown.
+    if (numVoters > 0) {
+      var timeout = game.timeout;
+      if (!game.timeout) {
+        timeout = Date.now() + 10000;
+        gameObj.update({timeout: timeout});
+      }
+      interval = setInterval(countdown, 100, timeout);
+    }
+    // If everyone voted or the countdown ran out, pick question and
+    // change state to respond.
+    console.warn('numVoters', numVoters, 'numPlayers', numPlayers,
+      'time expired?', Date.now() > game.timeout);
+    if (numVoters === numPlayers || Date.now() > game.timeout) {
+      clearInterval(interval);
+      var qDatas = [a, b, c];
+      var maxVotes = Math.max.apply(null, qDatas.map(qd => qd.voters.length));
+      var finalists = qDatas.filter(qd => qd.voters.length >= maxVotes);
+      var winner = finalists[Math.floor(Math.random() * finalists.length)];
+      console.warn('winner', winner.question);
+
+      gameObj.update({question: winner.question, state: "respond", timeout: null});
+    }
+  }
+  else {
     $("#responses").html("");
+    $('#question').html(game.question);
+    _showResponses();
   }
 
   if (playerObj !== null) {
     // Set current user class if the current window has a user
     $("#players ." + playerKey).addClass("current_user");
     // Determine which prompt to show to the user
-    if (state === "guess" && dbPlayers[playerObj.key()].ready >= round) {
+    if (state === "guess" && dbPlayers[playerObj.key()].responseReady >= round) {
       _showGuessedContainer();
     }
     else if (state === "respond") {
-      $("#responses").html("");
       _showSubmitContainer();
     }
   }
@@ -105,18 +203,110 @@ function handleUpdate(snapshot) {
   clientPlayers = dbPlayers;
 }
 
+function newGame() {
+  nameInput = $('#name').val().toLowerCase();
+  db.once('value').then(snapshot => {
+    var animal = "";
+    var currentAnimals = [];
+    snapshot.forEach(game => currentAnimals.push(game.val().animal));
+    // Keep trying to get an animal not currently in use
+    // TODO: Inefficient, stalls forever if all animals in use
+    do {
+      animal = data.animals[Math.floor(data.animals.length*Math.random())];
+    } while (currentAnimals.indexOf(animal) > 0);
+
+    gameObj = db.push({
+      round: 0,
+      state: "pick",
+      animal: animal
+    });
+    gameKey = gameObj.key();
+
+    window.location.hash = "/%g" + gameKey;
+
+    return prepareGame();
+  }).then(() => {
+    addPlayer(true);
+  });
+}
+
+function joinGame(playerStatus) {
+  var input = $('#game').val();
+  nameInput = $('#name').val().toLowerCase();
+  db.once('value').then(snapshot => {
+    var found = false;
+    snapshot.forEach(game => {
+      if (game.val().animal === input) {
+        found = true;
+        gameKey = game.key();
+        gameObj = snapshot.child(gameKey).ref();
+        window.location.hash = "/%g" + gameKey;
+      }
+    });
+    if (found) return prepareGame();
+  }).then(() => {
+    if (playerStatus !== "watcher") addPlayer(false);
+  }, error => {
+    alert('not found');
+  });
+}
+
+function watchGame() {
+  joinGame("watcher");
+}
+
+// Helper to prepare the game after it is found or created.
+function prepareGame() {
+  var loadBody = $.Deferred();
+  $(document.body).load("game.html", () => loadBody.resolve());
+
+  return loadBody.promise().then(() => {
+    gameObj.child("animal").once("value", snapshot => {
+      $("#info_container").html(snapshot.val());
+    });
+    $("#question_a").on('click', () => vote('a'));
+    $("#question_b").on('click', () => vote('b'));
+    $("#question_c").on('click', () => vote('c'));
+    gameObj.child("state").on("value", snapshot => state = snapshot.val());
+    gameObj.child("round").on("value", snapshot => round = snapshot.val());
+    gameObj.child("players").on("child_removed", snapshot => removePlayer(snapshot));
+    return gameObj.on("value", snapshot => handleUpdate(snapshot));
+  });
+}
+
 // Triggered when a player enters their name and submits
-function addPlayer() {
+function addPlayer(isHost) {
   // TODO: Disallow adding during guess round
-  var input = $('#name').val();
-  playerObj = players.push({
-    name: input, response: "", guessed: false, ready: 0
+  console.warn('about to push player');
+  playerObj = gameObj.child("players").push({
+    name: nameInput,
+    response: "",
+    guessed: false,
+    responseReady: 0,
+    voteReady: 0,
+    isHost: isHost
   });
   playerKey = playerObj.key();
-  window.location.hash = "/" + playerKey;
+  window.location.hash += "/%u" + playerKey;
   $("#players ." + playerKey).addClass("current_user");
-  $("#name_container").hide();
-  if (state !== "guess") _showSubmitContainer();
+  preparePlayer();
+}
+
+// Shows the correct player prompt on startup / player addition
+function preparePlayer() {
+  db.child(gameKey).child("players").child(playerKey).once("value", snapshot => {
+    // Show remove and settings buttons if player is host
+    console.warn('preparing player', snapshot.val(), console.trace());
+    if (snapshot.val().isHost) $('#settings_container').show();
+
+    // Show correct prompt at the top
+    if (state === "respond") {
+      _showSubmitContainer();
+    }
+    else if (state === "guess" && snapshot.val().responseReady === round) {
+      _showGuessedContainer();
+    }
+  });
 }
 
 function removePlayer(snapshot) {
@@ -130,33 +320,48 @@ function submit() {
   var input = $("#response").val();
   if (input === "") return;
   $("#response").val("");
-  playerObj.update({response: input, guessed: false, ready: round});
+  playerObj.update({response: input, guessed: false, responseReady: round});
 }
 
 function reportGuess() {
   playerObj.update({guessed: true});
 }
 
-function newRound() {
-  myRef.update({state: "respond", round: round+1});
+function vote(option) {
+  playerObj.update({
+    vote: option,
+    voteReady: round
+  });
 }
 
-// Toggles visibility of remove player buttons
-function toggleRemoveButtons() {
-  if ($(".remove").is(":visible")) $(".remove").hide();
-  else $(".remove").show();
+function newRound() {
+  var game = db.child(gameKey);
+  var qs = data.questions;
+  game.update({
+    state: "pick",
+    round: round+1,
+    questionA: _randomPick(qs),
+    questionB: _randomPick(qs),
+    questionC: _randomPick(qs)
+  });
+}
+
+function countdown(timeout) {
+  var ms = timeout - Date.now();
+  var seconds = Math.floor(ms/1000);
+  var tenths = Math.floor((ms/100)%10);
+  $("#timer").html(ms < 0 ? "0.0" : seconds + "." + tenths);
+  if (ms < 0) clearInterval(interval);
 }
 
 function _updatePlayerDOM(key, dbPlayer) {
   if (!(key in clientPlayers)) {
     // Player not in client
     $("#players").append(
-      _playerDOM(key, dbPlayer.name, dbPlayer.ready)
+      _playerDOM(key, dbPlayer)
     );
     // Add remove listener
-    $(".player."+key+" .remove").on('click', () => {
-      players.child(key).remove();
-    });
+    $(".player."+key+" .remove").on('click', () => playerObj.remove());
   }
   else {
     // Player in client
@@ -164,16 +369,16 @@ function _updatePlayerDOM(key, dbPlayer) {
     if (dbPlayer.name !== clientPlayer.name) {
       $("#players ."+key+" .name").html(dbPlayer.name);
     }
-    $("#players ."+key+" .ready").html(dbPlayer.ready < round ? "waiting" : "ready");
+    $("#players ."+key+" .ready").html(dbPlayer.responseReady < round ? "waiting" : "ready");
   }
 }
 
 // Returns a single instance of a player DOM item
-function _playerDOM(key, name, ready) {
-  ready = ready < round ? "waiting" : "ready";
+function _playerDOM(key, dbPlayer) {
+  var ready = dbPlayer.responseReady < round ? "waiting" : "ready";
   return "<div class='player "+key+"'>" +
-    "<span class='remove'>x</span>" +
-    "<span class='name'>"+name+"</span>" +
+    (dbPlayer.isHost ? "<span class='remove'>x</span>" : "") +
+    "<span class='name'>"+dbPlayer.name+"</span>" +
     "<span class='ready'>"+ready+"</span>" +
     "</div>";
 }
@@ -185,23 +390,44 @@ function _responseDOM(response, guessed) {
     guessed+"'>"+response+"</div><div class='response_triangle'></div></div>";
 }
 
-// Enter name and join button
-function _showNameContainer() {
-  $("#guessed_container").hide();
-  $("#submit_container").hide();
-  $("#name_container").show();
-}
-
 // Enter response and submit button
 function _showSubmitContainer() {
-  $("#name_container").hide();
   $("#guessed_container").hide();
   $("#submit_container").show();
 }
 
 // Button to indicate if response was guessed
 function _showGuessedContainer() {
-  $("#name_container").hide();
   $("#submit_container").hide();
   $("#guessed_container").show();
+}
+
+function _showQuestions() {
+  $("#responses").hide();
+  $("#questions").show();
+}
+
+function _showResponses() {
+  $("#questions").hide();
+  $("#responses").show();
+}
+
+// Returns a random element of the array
+function _randomPick(array) {
+  return array[Math.floor(Math.random()*array.length)];
+}
+
+// Found online, json parse function
+function _loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'data.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but
+      // simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
 }
