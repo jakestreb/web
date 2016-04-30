1ac53cb2af4857e45d70b2da5b8aec1ca3a4a387
Jake Streb <jake@getgrist.com>
2016-04-30 02:31:25 -0400

    Minor comment fix

--- things/index.js
+++ things/index.js
@@ -2,9 +2,6 @@
 var App = require('./App.js');
 var ko = require('knockout');
 
-// - Removing a player throws an error and causes issues sometimes (DOM fix should be in watch.html too)
-// - removing player messes up num sleeping
-
 // - Test with Safari and Firefox and Mobile
 // - Get more questions and filter out bad ones
 
